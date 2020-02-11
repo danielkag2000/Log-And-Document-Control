973aac6699566a3aaf86814dde7be4f45a64e5f0
Daniel <45756887+danielkag2000@users.noreply.github.com>
2020-02-11 12:18:21 +0200

    Add files via upload

---
+++ Log_And_Document_Control/JKCLog/JKCLog/JKCLog/IHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogSystem
+{
+    interface IHandler
+    {
+        /// <summary>
+        /// handlers
+        /// </summary>
+        /// <returns>State of the handler (falied, success and ect.)</returns>
+        string Handle();
+    }
+}
